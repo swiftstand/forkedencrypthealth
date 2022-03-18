779298ed27e36407427ff79d3d68cdff1367afee
kurbster <kkuznia28@gmail.com>
2022-03-18 12:07:59 -0500

    Removed django secret.

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5u%!9a=1($t!4eoii*qk#qx2txw*l&316g3he-khs7+-j%u5kd'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
