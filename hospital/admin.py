de541ed092a7d7d8d86201d98f5473b9e0f925e0
kurbster <kkuznia28@gmail.com>
2022-04-02 16:46:26 -0700

    Prems update.

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import Doctor, Insurance,Patient,Appointment,PatientDischargeDetails
 # Register your models here.
+class DoctorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Doctor, DoctorAdmin)
+
+#insurance created by prem
+class InsuranceAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Insurance, InsuranceAdmin)
+
+
+class PatientAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Patient, PatientAdmin)
+
+class AppointmentAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Appointment, AppointmentAdmin)
+
+class PatientDischargeDetailsAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(PatientDischargeDetails, PatientDischargeDetailsAdmin)
